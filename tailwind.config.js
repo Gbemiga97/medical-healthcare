f66b53a29445d9bfea63f92d03a472a9c5c2b873
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-11-18 11:24:45 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,}',
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px'
+      }
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1330px',
+    },
+    extend: {
+      colors: {
+        primary: '#242a2b',
+        secondary: '#808080',
+        accent: {
+          DEFAULT: '#1cbccf',
+          secondary: '#18abbc',
+          tertiary: '#90c6cd',
+        },
+        grey: '#e8f0f1',
+      },
+      fontFamily: {
+        primary: 'Poppins'
+      },
+      boxShadow: {
+        custom1: '0px 2px 40px 0px rgba(8, 70, 78, 0.08)',
+        custom2: '0px 0px 30px 0px rgba(8, 73, 81, 0.06)'
+      },
+      backgroundImage: {
+        services: 'url(/src/assets/img/services/bg.svg)',
+        testimonials: 'url(/src/assets/img/testimonials/bg.svg)',
+        department: 'url(/src/assets/img/department/bg.svg)',
+        quoteLeft: 'url(/src/assets/img/testimonials/quote-left.svg)',
+        quoteRight: 'url(/src/assets/img/testimonials/quote-right.svg)',
+      }
+    },
+  },
+  plugins: [],
+}
+
